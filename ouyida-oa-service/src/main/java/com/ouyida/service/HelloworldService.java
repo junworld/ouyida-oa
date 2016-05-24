076f4eb829f64c1b95b641e8e10bce620662e861
zhaojun33 <zhaojun5@jd.com>
2016-05-24 20:25:10 +0800

    init

---
+++ ouyida-oa-service/src/main/java/com/ouyida/service/HelloworldService.java
@@ -0,0 +1,7 @@
+package com.ouyida.service;
+
+/**
+ * Created by zhaojun5 on 2016/5/24.
+ */
+public class HelloworldService {
+}
---
+++ ouyida-oa-web/src/main/java/com/ouyida/web/action/HelloworldAction.java
@@ -0,0 +1,11 @@
+package com.ouyida.web.action;
+
+/**
+ * Created by zhaojun5 on 2016/5/24.
+ */
+public class HelloworldAction {
+
+    public static void main(String[] args) {
+
+    }
+}
